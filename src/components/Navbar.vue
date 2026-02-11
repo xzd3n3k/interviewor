fab48c58ef979d81cca05f4a5cf67a7c54ab1735
Zdeněk Němec <xzdeneknemec@outlook.cz>
2026-02-11 19:53:53 +0100

    feat(home): add navbar, and basic layout of home

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  import logo from "@/assets/img/logo.png";
+</script>
+
+<template>
+  <nav class="flex items-center justify-between px-6 py-4 w-full z-50 font-light">
+
+    <ul class="flex items-center gap-16  text-base">
+      <img :src="logo" alt="Logo" class="h-18" />
+      <li class="cursor-pointer">Companies</li>
+      <li class="cursor-pointer">Reviews</li>
+      <li class="cursor-pointer">Submit</li>
+      <li class="cursor-pointer text-[#1C2E4A] rounded-2xl bg-white px-3 py-1">More</li>
+    </ul>
+
+    <div class="flex flex-row gap-3">
+      <button>Sign In</button>
+      <button>Join</button>
+    </div>
+
+  </nav>
+</template>
+
+<style scoped>
+  button {
+    cursor: pointer;
+    border: 1px solid black;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+  import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  Toto je home
+  <!-- Hero -->
+  <section
+      id="home"
+      class="relative min-h-screen w-full bg-no-repeat bg-cover bg-center"
+  >
+    <Navbar />
+    <div class="font-bold text-6xl flex flex-col gap-2 justify-center items-center text-center">
+
+      Know the truth about where you want to apply
+
+    </div>
+  </section>
+
+  <!-- Features -->
+  <section id="about" class="bg-white w-full min-h-screen p-8">
+    <h2 class="text-3xl font-semibold mb-4">Features</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scroll down to see more content.
+    </p>
+  </section>
 </template>
 
 <style scoped>
+.logo {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+}
 </style>
