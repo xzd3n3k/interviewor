b43826bd639659b15e1accd1878a423ff1240bf4
Zdeněk Němec <xzdeneknemec@outlook.cz>
2026-02-11 20:59:09 +0100

    feat(navbar): minor ux ui changes to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,17 +5,17 @@
 <template>
   <nav class="flex items-center justify-between px-6 py-4 w-full z-50 font-light">
 
-    <ul class="flex items-center gap-16  text-base">
+    <ul class="flex items-center gap-10  text-base">
       <img :src="logo" alt="Logo" class="h-18" />
-      <RouterLink class="cursor-pointer" to="/companies">Companies</RouterLink>
-      <RouterLink class="cursor-pointer" to="">Reviews</RouterLink>
-      <RouterLink class="cursor-pointer" to="">Submit</RouterLink>
-      <RouterLink class="cursor-pointer text-[#1C2E4A] rounded-2xl bg-white px-3 py-1" to="">More</RouterLink>
+      <RouterLink to="/companies">Companies</RouterLink>
+      <RouterLink to="">Reviews</RouterLink>
+      <RouterLink to="">Submit</RouterLink>
+      <RouterLink to="">More</RouterLink>
     </ul>
 
     <div class="flex flex-row gap-3">
-      <button>Sign In</button>
-      <button>Join</button>
+      <button class="border border-black px-3 py-1 font-medium">Sign In</button>
+      <button class="bg-black text-white px-3 py-1 font-medium">Join</button>
     </div>
 
   </nav>
@@ -25,5 +25,9 @@
   button {
     cursor: pointer;
     border: 1px solid black;
+  }
+
+  RouterLink {
+    cursor: pointer;
   }
 </style>
